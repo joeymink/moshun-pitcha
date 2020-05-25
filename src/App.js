11b436c855f156b3510fca0ac9038f282ff528e3
Joey Mink <joeymink@gmail.com>
2020-05-25 18:11:01 -0400

    implement rough solution for handling configuration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+  App Configuration
+ **/
+
+let config = {
+  giphy_api_key: 'your_api_key',  
+};
+
+try {
+  const local_config = require('./local-config.js').default;
+  config = {...config, ...local_config};
+} catch (ex) {
+  console.warn('No local config found.');
+}
+
 function App() {
   return (
     <div className="App">
