e7a38639be8308e9dcc0efd14bb64cae6ad91dd8
Joey Mink <joeymink@gmail.com>
2020-05-28 07:36:44 -0400

    add test for search form component

---
+++ src/components/search-form.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { SearchForm } from './search-form.js';
+
+test('form submission calls expected function', () => {
+  const onSubmitFn = jest.fn();
+  const wrapper = mount(<SearchForm onSearchSubmit={onSubmitFn}/>);
+  const form = wrapper.find('form');
+  form.simulate('submit', {
+    target: {
+      search_query: {
+        value: 'my search query'
+      }
+    }
+  });
+  expect(onSubmitFn).toHaveBeenCalledTimes(1);
+});
